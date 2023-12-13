14587e308dd458821e4c5f494510342787d1ba67
MaximKorolyov <92077049+MaximKorolyov@users.noreply.github.com>
2023-12-14 01:39:42 +0300

    Delete ESB_copy/ESB directory

--- ESB_copy/ESB/components/list.js
+++
@@ -1,8 +0,0 @@
-// CREATE TABLE "cars" (
-// 	"id"	INTEGER NOT NULL,
-// 	"name"	TEXT NOT NULL,
-// 	"mileage"	TEXT,
-// 	"year"	INTEGER,
-// 	"color"	TEXT,
-// 	PRIMARY KEY("id" AUTOINCREMENT)
-// );
